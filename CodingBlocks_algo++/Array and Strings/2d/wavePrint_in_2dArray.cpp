10367e19c1ddf0fc58922d5372ae630e9af097fb
shubham Halder <44200751+888connor888@users.noreply.github.com>
2019-12-30 01:10:37 +0530

    Create wavePrint_in_2dArray.cpp

---
+++ CodingBlocks_algo++/Array and Strings/2d/wavePrint_in_2dArray.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+# define R 10
+# define C 10
+
+void wavePrint(int a[R][C],int m,int n){
+	int i=0,j=0,wave_direction=1;
+    while(j<n){
+    	if(wave_direction==1){
+        for(int i=0;i<m;i++){
+        	cout<<a[i][j]<<" ";
+        }
+        wave_direction=0;
+        j++;
+    	}
+    	if(wave_direction==0){
+        for(int i=m-1;i>=0;i--){
+        	cout<<a[i][j]<<" ";
+        }
+        wave_direction=1;
+        j++;
+    	}
+    }
+}
+
+
+int main(){
+	int m,n;
+	int a[R][C];
+	cin>>m>>n;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			cin>>a[i][j];
+		}
+	}
+	wavePrint(a,m,n);
+	return 0;
+}
